ecf5795165e948f714b07daba65db6d8270e0648
Minyue Jiang <623583576@qq.com>
2016-12-02 19:56:31 +0800

    Update experiments configuration

---
+++ experiments/eval/voc2007_test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+python ./faster_rcnn/test_net.py \
+--gpu 0 \
+--weights ./output/faster_rcnn_end2end/voc_2007_trainval/VGGnet_fast_rcnn_iter_80000.ckpt \
+--imdb voc_2007_test \
+--cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
+--network VGGnet_test
+
+
+python ./faster_rcnn/test_net.py \
+--gpu 0 \
+--weights ./output/faster_rcnn_end2end/voc_2007_trainval/VGGnet_fast_rcnn_iter_90000.ckpt \
+--imdb voc_2007_test \
+--cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
+--network VGGnet_test
